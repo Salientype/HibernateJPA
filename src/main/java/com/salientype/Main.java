ca59261dd1023965fd2ad04ee15e7db2732bfbc1
Jorge Alvarez <jorge@salientype.com>
2025-01-22 15:07:28 -0500

    Assignment completed.

---
+++ src/main/java/com/salientype/Main.java
@@ -0,0 +1,26 @@
+package com.salientype;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import static com.salientype.controller.UserController.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+
+        // addUser(factory,session);
+
+        // findUser(factory,session,2);
+
+        // updateUser(session,3);
+
+        deleteUser(session,4);
+
+    }
+}
